a3f94aa17f9963ff4f59a88b05ae22efc74287ec
RohidNawa <rohid.m12@gmail.com>
2025-06-11 02:42:08 +0800

    Membuat class matematikaberaksi

--- src/hitung/matematikaBeraksi.java
+++ src/hitung/matematikaBeraksi.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hitung;
 
-/**
- *
- * @author rohid
- */
+
 public class matematikaBeraksi {
-    
+    public static void main(String[] args) {
+        matematika rohid = new matematika(4,7);
+        
+        System.out.println("Hasil Penjumlahan : "+rohid.setPenjumlahan());
+        System.out.println("Hasil pengurangan : "+rohid.setPengurangan());
+        System.out.println("Hasil perkalian : "+rohid.setPerkalian());
+        System.out.println("Hasil pembagian : "+rohid.setPembagian());
+    }
 }
