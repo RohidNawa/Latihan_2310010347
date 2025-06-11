4af0d84c382dbbcf528db59853c4c511f710bc5f
RohidNawa <rohid.m12@gmail.com>
2025-06-11 15:24:58 +0800

    initial commit

---
+++ src/latihan2/DataDiri.java
@@ -0,0 +1,12 @@
+
+package latihan2;
+
+public class DataDiri {
+    private String nama;
+    private String npm;
+     private String kelas;
+     
+     public DataDiri(String nama, String npm, String kelas){
+     }
+    
+}
