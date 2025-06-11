d6fb49bb9723ad60ff137b3d7ff3721ec26bab01
RohidNawa <rohid.m12@gmail.com>
2025-06-11 15:37:00 +0800

    membuat objek

--- src/latihan2/DataDiri.java
+++ src/latihan2/DataDiri.java
@@ -2,11 +2,20 @@
 package latihan2;
 
 public class DataDiri {
-    private String nama;
-    private String npm;
-     private String kelas;
+     String nama;
+     String npm;
+      String kelas;
      
      public DataDiri(String nama, String npm, String kelas){
+         this.nama = nama;
+         this.npm = npm;
+         this.kelas = kelas;
+     }
+     
+     public void tampilData(){
+     System.out.println("Nama : "+ nama);
+     System.out.println("NPM : "+ npm);
+     System.out.println("Kelas : "+ kelas);
      }
     
 }
---
+++ src/latihan2/DataDiriBeraksi.java
@@ -0,0 +1,11 @@
+
+package latihan2;
+
+
+public class DataDiriBeraksi {
+    public static void main(String[] args) {
+        DataDiri saya = new DataDiri("Rohid", "2310010347", "4C Reguler Banjarbaru");
+        
+        saya.tampilData();
+    }
+}
